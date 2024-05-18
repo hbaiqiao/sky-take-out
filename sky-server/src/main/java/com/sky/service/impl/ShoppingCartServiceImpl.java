2c911b305ad83bcbddcdb35c248975e28dcd663d
hbaiqiao <1343486201@qq.com>
2024-05-18 17:20:51 +0800

    清空购物车功能代码

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -75,4 +75,27 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
 
    }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    public  List<ShoppingCart> showShoppingCart(){
+        //获取当前微信用户id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(userId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        return list;
+    }
+
+    /**
+     * 清空购物车
+     */
+    public void cleanShoppingCart(){
+        Long userId = BaseContext.getCurrentId();
+        shoppingCartMapper.deleteByUserId(userId);
+    }
 }
